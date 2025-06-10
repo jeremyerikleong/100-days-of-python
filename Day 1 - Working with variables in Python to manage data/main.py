65bd8aa0c06ae7ec3dd08ec84b38e40e16a75fcc
Jeremy <jeremythegreat95@gmail.com>
2025-06-10 22:41:17 +0800

    working with variables, basic python function and syntax

---
+++ Day 1 - Working with variables in Python to manage data/main.py
@@ -0,0 +1,11 @@
+print("Hello, Welcome to Python Playground")
+
+name = input("What is your name?\n")
+print("Hello " + name)
+
+# comment line
+# Indentation in Python is not just for readability; it is a fundamental part of the syntax.
+# print function - prints the specified message to the screen, or other standard output device.
+# input function - to get input from the user
+# len function - to get string length
+# \n create a new line
