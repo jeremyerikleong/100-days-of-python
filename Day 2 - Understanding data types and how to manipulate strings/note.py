adc010122811c1a4cac84b153c86b8e173368692
Jeremy <jeremythegreat95@gmail.com>
2025-06-12 09:05:52 +0800

    update notes

--- Day 2 - Understanding data types and how to manipulate strings/note.py
+++ Day 2 - Understanding data types and how to manipulate strings/note.py
@@ -1,18 +1,25 @@
-## Data Types
+## Primitive Data Types
 # Strings - "Hello Python"
-# Large Integers - 123,456,789
 # Integers - 12345
 # Float - 1.68
 # Boolean - True / False
 
 # places [] behind a string to dissect the string
-# subscripting - print("Python[0]")
+# subscripting - print("Python[0]"), it starts with[0] - binary 0, 1
 
 # type function - to determining the type of the object
 # type conversion - using built in int() to convert anything to numbers
 
-
 ## Mathematical Operations
-# order of priority - PEMDAS
+# PEMDAS (order of priority)
+# Parenthesis ( )
+# Exponential **
+# Multiple *
+# Divide /
+# Addition +
+# Subtraction -
+
+# round function - to round number off
+# floor division -  is an arithmetic operation that divides two numbers and returns the largest possible integer that is less than or equal to the result of the division. It is performed using the // operator.
 
 # fstrings works like string literals in javascript
