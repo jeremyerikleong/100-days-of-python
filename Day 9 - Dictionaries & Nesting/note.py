5ac6b509fd459ce93a55a8340d6a395ac5ba09b9
Jeremy <jeremythegreat95@gmail.com>
2025-06-14 21:49:37 +0800

    build grading program

---
+++ Day 9 - Dictionaries & Nesting/note.py
@@ -0,0 +1,10 @@
+# dictionaries in python comes with key and value pairs  e.g. {key: value}
+# python dictionaries and javascript objects share similarities
+
+frameworks = {
+    "vue": "vue is a progressive javascript framework for building user interfaces and single page applications",
+    "tailwind": "tailwind css is an open source css framework.",
+}
+
+for key in frameworks:
+    print(frameworks[key])
